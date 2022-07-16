6c6be31d1c6b79312032b744cfc37c0f7e013622
Tamru <tamrualem111@gmail.com>
2022-07-16 16:51:17 +0300

    update

--- test.js
+++
@@ -1,9 +0,0 @@
-const hamburgerBtn = document.getElementById('hamburgerBtn');
-const navBar = document.getElementById('navBar');
-
-
-    hamburgerBtn.addEventListener('click', () => {
-        navBar.classList.toggle('open');
-
-    });
-
